754fb801d22991822c6ff9eaf0f4f1c3ad14efe9
tooba GG <dsereduk@gmail.com>
2023-01-29 21:55:11 -0700

    removed some old js functionality

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,37 +4,6 @@ import "../Dashboard.css";
 import { Link } from "react-router-dom";
 
 
-const DisplayAll = () => {
-    const tasks=["High", "Medium", "Low"];
-    const [allTasks, setAllTasks] = useState([]);
-    useEffect(() => {
-        axios
-        .get("http://localhost:8000/api/task")
-        .then((response) => {
-            console.log(response.data);
-            setAllTasks(response.data);
-        })
-        .catch((err) => {
-            console.log(err.response);
-        });
-    }, []);
-
-    const handleDeleteTask = (idFromBelow) => {
-        axios
-        .delete(`http://localhost:8000/api/task/${idFromBelow}`)
-        .then((response) => {
-            console.log("successfully removed task");
-            console.log(response);
-            const filteredTasks = allTasks.filter((task) => {
-            return task._id !== idFromBelow;
-            });
-            setAllTasks(filteredTasks);
-        })
-        .catch((err) => {
-            console.log("error deleting task", err.response);
-        });
-    };
-
     return (
       <>
         <body>
@@ -44,7 +13,6 @@ const DisplayAll = () => {
         </body>
       </>
     );
-    };
 
 export default DisplayAll;
 
