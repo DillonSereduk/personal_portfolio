434b3bab21619457fcde426736502631553ec855
tooba GG <dsereduk@gmail.com>
2023-01-31 00:04:08 -0700

    Styling!

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,15 +4,123 @@ import "../Dashboard.css";
 import { Link } from "react-router-dom";
 
 
+const DisplayAll = () => {
+    const tasks=["High", "Medium", "Low"];
+    const [allTasks, setAllTasks] = useState([]);
+    useEffect(() => {
+        axios
+        .get("http://localhost:8000/api/task")
+        .then((response) => {
+            console.log(response.data);
+            setAllTasks(response.data);
+        })
+        .catch((err) => {
+            console.log(err.response);
+        });
+    }, []);
+
+    const handleDeleteTask = (idFromBelow) => {
+        axios
+        .delete(`http://localhost:8000/api/task/${idFromBelow}`)
+        .then((response) => {
+            console.log("successfully removed task");
+            console.log(response);
+            const filteredTasks = allTasks.filter((task) => {
+            return task._id !== idFromBelow;
+            });
+            setAllTasks(filteredTasks);
+        })
+        .catch((err) => {
+            console.log("error deleting task", err.response);
+        });
+    };
+
     return (
       <>
         <body>
-          <div className="header">
-            
-          </div>
+          <section class="black">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          <section class="maroon">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          <section class="rust">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          <section class="tomato">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section >
+          <section class="coral">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          <section class="vermilion">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          <section class="scarlet">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          <section class="bright_red">
+            <h1>Nice Curves</h1>
+              <p>
+                In incididunt nisi nisi fugiat Lorem labore id laboris dolore sunt. 
+                Magna irure cillum amet cupidatat eu do occaecat cupidatat mollit reprehenderit est fugiat. 
+                Aliqua excepteur proident fugiat sunt do. Velit incididunt non in dolor enim et aliqua. 
+                Laborum id minim fugiat id sint labore nulla nulla.
+              </p>
+          </section>
+          
+          
+          
+          
+          
+          
+          
         </body>
       </>
     );
+  };
 
 export default DisplayAll;
 
