5fbc78b474fe9909ff163a286a821f2cf1dd3684
tooba GG <dsereduk@gmail.com>
2023-02-03 23:04:55 -0700

    edit

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,37 +4,7 @@ import "../Dashboard.css";
 import { Link } from "react-router-dom";
 
 
-const DisplayAll = () => {
-    const tasks=["High", "Medium", "Low"];
-    const [allTasks, setAllTasks] = useState([]);
-    useEffect(() => {
-        axios
-        .get("http://localhost:8000/api/task")
-        .then((response) => {
-            console.log(response.data);
-            setAllTasks(response.data);
-        })
-        .catch((err) => {
-            console.log(err.response);
-        });
-    }, []);
-
-    const handleDeleteTask = (idFromBelow) => {
-        axios
-        .delete(`http://localhost:8000/api/task/${idFromBelow}`)
-        .then((response) => {
-            console.log("successfully removed task");
-            console.log(response);
-            const filteredTasks = allTasks.filter((task) => {
-            return task._id !== idFromBelow;
-            });
-            setAllTasks(filteredTasks);
-        })
-        .catch((err) => {
-            console.log("error deleting task", err.response);
-        });
-    };
-
+function App() {
     return (
       <>
         <body>
@@ -105,7 +75,7 @@ const DisplayAll = () => {
     );
   };
 
-export default DisplayAll;
+export default App;
 
           {/* <section class="nav-bar-main">
             <section class="nav-bar-left">
